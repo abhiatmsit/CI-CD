96ea61f3cd88c67832240fd8d3046a3c37cab378
Abhishek Kumar <35526848+abhiatmsit@users.noreply.github.com>
2022-07-18 14:57:35 +0530

    Create scripts.groovy
    external groovy script

---
+++ scripts.groovy
@@ -0,0 +1,16 @@
+def build(){
+	echo 'building the application'
+}
+
+def test(){
+		echo 'building the application'
+}
+
+def deploy(){
+	
+	echo 'building the application'
+	echo "deploying versio ${params.VERSION}"
+	
+}
+
+return this
